3c04dcd865102b2fcdbb1a392cae00af1ff00494
Alexei Ruiz <alexeiruizrodri@gmail.com>
2022-11-21 21:36:47 -0300

    Toast

--- frontend/src/assets/components/NotificationButton/index.tsx
+++ frontend/src/assets/components/NotificationButton/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import icone from "../../img/notificacion-icon.svg";
+import { toast } from "react-toastify";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
 
@@ -8,7 +9,7 @@ type Props = {
 };
 function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
-    console.log("SUCESSO");
+    toast.info("SMS enviado com sucesso");
   });
 }
 function NotificationButton({ saleId }: Props) {
